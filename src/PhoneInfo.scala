626e31fd3193aaf483d2d07eea6f0aa39b7b1c6a
hyunkyuchang <hkchang.sjhstudio@gmail.com>
2024-06-03 12:26:30 +0900

    1. 불필요한 세미클론 제거 2. 불필요한 중괄호 제거 3. PhoneInfoFunction > pattern 을 private으로 재정의 4. PhoneInfo toString에 반환 타입 정의

--- src/PhoneInfo.scala
+++ src/PhoneInfo.scala
@@ -1,22 +1,22 @@
 class PhoneInfo private (private var name:String, private var addr:String,private var phoneNo:String) {
-  override def toString = {
-    s"Name: ${name} / Address: ${addr} / Phone No:${phoneNo}"
+  override def toString: String = {
+    s"Name: $name / Address: $addr / Phone No:$phoneNo"
   }
   //getter
-  def getName:String = name;
-  def getAddr:String = addr;
-  def getPhoneNo:String = phoneNo;
+  def getName:String = name
+  def getAddr:String = addr
+  def getPhoneNo:String = phoneNo
 
   //Setter
   def changeInfo(name:String,addr:String,phoneNo:String):Unit={
     if(!name.isBlank){
-      this.name = name;
+      this.name = name
     }
     if(!addr.isBlank){
       this.addr=addr
     }
     if(!addr.isBlank && PhoneInfoFunction.chkPhoneNo(phoneNo)){
-      this.phoneNo = phoneNo;
+      this.phoneNo = phoneNo
     }
   }
 }
--- src/PhoneInfoFunction.scala
+++ src/PhoneInfoFunction.scala
@@ -1,6 +1,6 @@
 object PhoneInfoFunction {
-  val pattern = "^\\d{3}-\\d{4}-\\d{4}$".r
-  val PhoneBook:Map[String,PhoneInfo] = Map[String,PhoneInfo]().empty;
+  private val pattern = "^\\d{3}-\\d{4}-\\d{4}$".r
+  val PhoneBook:Map[String,PhoneInfo] = Map[String,PhoneInfo]().empty
   def chkPhoneNo(phoneNo:String):Boolean=
   {
     pattern.matches(phoneNo)
